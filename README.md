f2b64574af9af34d3bada022f59a433a650a5d48
anggasayogo <anggasayogosm@gmail.com>
2020-10-21 09:13:07 +0700

    create docs instalation

---
+++ README.md
@@ -0,0 +1,27 @@
+# CRUD PHP & MYSQL
+ waht it's Crud ? (Create read Update dan delete ) dalam pemrograman ada 4 fungsi dasar dalam penyimpanan persisten. yaitu create untuk membuat data atau memasukan data ke database, read untuk melihat data atau get data, update yaitu untuk mengupdate data yang sudah ada di databse lalau kita update lagi ke database, dan delete yaitu cara menghapus data yang ada di database
+
+## requirment/syarat
+- [PHP > 7.0](https://php.net)
+- [Mysql](https://www.mysql.com)
+- [Webserver](https://www.nginx.com)
+- [or Cross-platform XAMPP](https://www.apachefriends.org)
+
+## instalasi
+
+download script atau clone repo ini dengan cara
+```
+git clone https://github.com/Anggasayogo/UMB-2020-phpmysql.git
+```
+lalu buat nama databse
+```
+umb_2020
+```
+import file umb_2020.sql ke databse yang sudah dibuat
+dan jalankan di :
+```
+localhost/namafilerscript
+```
+
+## happy success instalation
+![bg](image/bg.jpeg)
---
+++ umb_2020.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Okt 2020 pada 03.54
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `umb_2020`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` char(15) NOT NULL,
+  `jenis_kelamin` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `jenis_kelamin`) VALUES
+(5, 'Angga Maulana', 'laki-laki');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
